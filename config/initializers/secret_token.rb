3f68c8c4cb9377d8ac0537a1caa79a99deb84943
Jeff Miller <loadeddesigns@gmail.com>
2013-03-24 23:09:09 -0700

    Create fresh Rails 4.0.0.beta1 App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HumaniSe::Application.config.secret_key_base = '7877814882dc0d9800475b729720102ffa6dc4cfc7e9d2e4fa38b0beacfad5e1255475f5129439961e2d44f66ed7e97eda6749c451f9774a719c6c292de02d8f'
