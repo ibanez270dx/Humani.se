d0ae86e8a13268e7b8d96149887a0d399a51c24c
Jeff Miller <loadeddesigns@gmail.com>
2014-12-14 21:57:37 -0800

    Overhaul admin section, use pureCSS framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   def flash_class(level)
     case level
     when :notice then "alert alert-info"
@@ -8,7 +8,7 @@ module ApplicationHelper
     when :alert then "alert alert-error"
     end
   end
-  
+
   def human_date(date)
     suffix = case date.day
     when 1 then "st"
@@ -16,18 +16,19 @@ module ApplicationHelper
     when 3 then "rd"
     else "th"
     end
-    
+
     date.strftime("%A, %B %e#{suffix} %Y")
   end
-  
-  #OPTIMIZE ...didn't really want to add the Filepicker gem just for these two methods
+
+  # OPTIMIZE
+  # ...didn't really want to add the Filepicker gem just for these two methods
   def filepicker_image_tag(url, image_options={}, image_tag_options={})
     image_tag(filepicker_image_url(url, image_options), image_tag_options)
   end
-  
+
   def filepicker_image_url(url, options = {})
     query_params = options.slice(:w, :h, :fit, :align, :crop, :format, :quality, :watermark, :watersize, :waterposition).to_query
     [url, "/convert?", query_params].join
   end
-  
+
 end
