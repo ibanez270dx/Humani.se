903ed90d120a3673c800af8a729ac6ac3df68c81
Jeff Miller <jeff@humani.se>
2016-06-15 22:59:56 -0700

    remove old app server configuration, add rbenv-vars as linked file

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :use_sudo, false
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.rbenv-vars')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
