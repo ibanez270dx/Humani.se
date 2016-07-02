0717a7fc914b996dba664349a0d3bfd3311f6a79
Jeff Miller <jeff@humani.se>
2016-07-02 00:06:30 -0700

    Remove unused helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,23 +19,29 @@ module ApplicationHelper
     date.strftime("%A, %B %e#{suffix} %Y")
   end
 
+  def nav_link_to(name, href, options={})
+    selected = [controller_name, action_name].join('#').match(/^#{options.delete(:selected)}/)
+    options[:class] = "selected #{options[:class]}" if selected
+    link_to name, href, options
+  end
+
   def bg(photo_url)
     "background-image: url(#{photo_url});"
   end
 
-  def icon(icon, icon_text=nil)
-    content_tag(:i, icon_text, class: "hu-#{icon}")
-  end
+  # def icon(icon, icon_text=nil)
+  #   content_tag(:i, icon_text, class: "hu-#{icon}")
+  # end
+  #
+  # def icon_link(icon = nil, html_options = {}, &block)
+  #   icon_text = html_options.delete(:icon_text)
+  #   html_options.transform_keys! { |key| /data_(\w+)/ === key ? key.to_s.dasherize : key }
+  #   content_tag(:a, nil, html_options) { icon(icon, icon_text) }
+  # end
 
-  def icon_link(icon = nil, html_options = {}, &block)
-    icon_text = html_options.delete(:icon_text)
-    html_options.transform_keys! { |key| /data_(\w+)/ === key ? key.to_s.dasherize : key }
-    content_tag(:a, nil, html_options) { icon(icon, icon_text) }
-  end
-
-  def cutout_icon(icon, text=nil)
-    content_tag(:a, nil, class: "#{icon} hu-#{icon}-cutout") { icon(icon, text) }
-  end
+  # def cutout_icon(icon, text=nil)
+  #   content_tag(:a, nil, class: "#{icon} hu-#{icon}-cutout") { icon(icon, text) }
+  # end
 
   def social_count(network, count)
     content_tag(:a, nil, class: network) do
