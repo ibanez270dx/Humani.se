66e7fa31d3cc2d06d550410a5d45339d0c2dc881
Jeff Miller <jeff@humani.se>
2017-10-27 08:30:59 -0700

    Clean up deploy settings

--- Capfile
+++ Capfile
@@ -1,16 +1,13 @@
 # Load DSL and set up stages
 require 'capistrano/setup'
-
-# Include default deployment tasks
 require 'capistrano/deploy'
 
 require 'capistrano/rbenv'
-require 'capistrano/bundler'
-require 'capistrano/rails/assets'
-require 'capistrano/rails/migrations'
+require 'capistrano/rails'
 require 'capistrano/nginx'
 require 'capistrano/puma'
-require 'capistrano/puma/nginx'
+install_plugin Capistrano::Puma
+install_plugin Capistrano::Puma::Nginx
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,27 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock "3.10.0"
 
 # Name and Repo
-set :application, 'humani.se'
-set :repo_url, 'git@github.com:ibanez270dx/humani.se.git'
+set :application, "humani.se"
+set :repo_url, "git@github.com:ibanez270dx/humani.se.git"
+set :branch, :no_default_branch
 
 # App Location
-set :deploy_to, '/srv/humani.se'
-set :deploy_via, :copy
-
-# Deploy User
-set :user, 'deploy'
-set :use_sudo, false
+set :user, "deploy"
+set :deploy_to, "/home/#{fetch(:user)}/humani.se"
 
 # Default value for :pty (pseudo terminal) is false
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.rbenv-vars')
+set :linked_files, fetch(:linked_files, []).push("config/database.yml", "config/secrets.yml")
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
 
-# RbEnv
+# Rbenv
 set :rbenv_type, :user
-set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_ruby, File.read(".ruby-version").strip
 
 # Puma
 set :puma_init_active_record, true
